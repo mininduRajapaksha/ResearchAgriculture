a25c4e41a29fdad5adf5a76fe2272c6f7613f09b
IT21839160 <it21839160@my.sliit.lk>
2025-03-16 10:40:49 +0530

    data augmentation and preprocessing

--- model.py
+++ model.py
@@ -11,3 +11,36 @@ train_dir = 'D:/Research project/Datasets/Banana Dataset/Train'
 test_dir = 'D:/Research project/Datasets/Banana Dataset/Test'
 valid_dir = 'D:/Research project/Datasets/Banana Dataset/Valid'
 
+#data augmentation and preprocessing
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# Load training dataset
+train_set = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(416, 416),
+    batch_size=32,
+    class_mode='categorical')
+
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+# Load validation dataset
+val_set = val_datagen.flow_from_directory(
+    valid_dir,
+    target_size=(416, 416),
+    batch_size=32,
+    class_mode='categorical')
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# Load test dataset
+test_set = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(416, 416),
+    batch_size=32,
+    class_mode='categorical')
+
+
