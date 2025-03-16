7b3a2b179a1791535e51d12c9df3c203be25beb4
IT21839160 <it21839160@my.sliit.lk>
2025-03-16 08:03:24 +0530

    defining folder paths

---
+++ model.py
@@ -0,0 +1,11 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
+# import cv2
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+#defining the paths
+train_dir = 'D:\Research project\Datasets\Banana Dataset\Train'
+test_dir = 'D:\Research project\Datasets\Banana Dataset\Test'
+valid_dir = 'D:\Research project\Datasets\Banana Dataset\Valid'
